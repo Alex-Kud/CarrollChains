af7ef658c80c962db15aee3291e2ca52dd24c337
alexander.kudashov <sasha.kudaschov2014@yandex.ru>
2022-05-27 15:59:43 +0300

    Добавьте файлы проекта.

---
+++ CarrollChains/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace CarrollChains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\tДополнительное задание по ООП.\n" +
+                "Решение задачи \"Метаграмма\" для поиска оптимального пути цепочек Кэрролла");
+            Console.Write("Введите размер словаря:");
+            int matrixSize = Convert.ToInt32(Console.ReadLine());
+            string[] dictionary = new string[matrixSize];
+           
+            // Считывание словаря
+            for (int i = 0; i < matrixSize; i++)
+            {
+                //Console.Write($"Введите {i}-ое слово словаря: ");
+                dictionary[i] = Console.ReadLine();
+            }
+
+            int[,] matrix = GetMatrix(dictionary, matrixSize);
+            PrintMatrix(matrix, matrixSize);
+
+
+
+            ShortestPathFinder test = new(matrix, matrixSize);
+            test.MatrixPrint();
+            test.Dijkstra(matrix, 0);
+        }
+
+        /// <summary>
+        /// Матрица смежности графа возможных преобразований слов
+        /// </summary>
+        /// <param name="dictionary">Словарь слов</param>
+        /// <param name="matrixSize">Длина словаря</param>
+        /// <returns></returns>
+        static int[,] GetMatrix(string[] dictionary, int matrixSize)
+        {
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    // Заполнение весов переходов из вершины в саму себя
+                    if (i == j)
+                    {
+                        matrix[i, j] = 0;
+                        continue;
+                    }
+                    // Заполнение весов переходов из вершины во все другие вершины
+                    // Если переход за 1 шаг возможен - вес = 1, если не возможен, вес = 0
+                    matrix[i, j] = (HasOneDiff(dictionary[i], dictionary[j])) ? 1 : 0;
+                }
+            }
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// Проверка на то, имеет ли 2 строку разницу лишь в одном символе
+        /// </summary>
+        /// <param name="BaseString">Исходная строка</param>
+        /// <param name="StringToCountDiff">Строка, разница с которой проверяется</param>
+        /// <returns></returns>
+        static bool HasOneDiff(string BaseString, string StringToCountDiff)
+        {
+            int diffCount = 0;
+
+            if (BaseString.Length == StringToCountDiff.Length)
+            {
+                for (int i = 0; i < BaseString.Length; i++)
+                {
+                    if (BaseString[i] != StringToCountDiff[i])
+                    {
+                        diffCount++;
+                    }
+                }
+                    return diffCount == 1;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Печать матрицы переходов
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="matrixSize"></param>
+        static void PrintMatrix(int[,] matrix, int matrixSize)
+        {
+            Console.WriteLine("Вывод матрицы");
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
